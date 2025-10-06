c083c684c33318c5952a4d1ad6f4abf362393e92
Caleb Peterson <caleb.peterson@cubicle6.com>
2025-10-05 21:14:55 -0400

    clean up Introduction screen layout

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -13,16 +13,19 @@ export const Introduction: FC = () => (
     </div>
 
     <div>
-      Scoundrel is a solo dungeon-crawling card game. Each room is made up of
-      four cards representing monsters, weapons, and healing items. You must
-      choose three cards to resolve in any order and carry the last card to the
-      next room.
+      Scoundrel is a solo rogue-like card game. Each room is made up of four
+      cards representing monsters, weapons, and healing items. You must choose
+      three cards to resolve in any order and carry the last card to the next
+      room.
     </div>
+
     <div>
       Spades and clubs are monsters that deal damage. Diamonds are weapons that
       help you fight stronger monsters. Hearts heal your health.
     </div>
+
     <div>You can run from a room once, but not twice in a row.</div>
+
     <div>
       The game ends when you die (your health hits zero) or survive through the
       deck. Success depends on smart risk-taking and order of play.
