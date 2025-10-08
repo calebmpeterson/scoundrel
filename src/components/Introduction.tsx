54f440721d05ba62edb4b1daf7bc7d220c8988f4
Caleb Peterson <caleb.peterson@cubicle6.com>
2025-10-08 08:04:59 -0400

    update styles/colors

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,22 +1,19 @@
-import type { FC } from "react";
+import type { CSSProperties, FC } from "react";
 import styles from "./Introduction.module.css";
+
+const COLOR = "--color";
 
 export const Introduction: FC = () => (
   <div className={styles.container}>
+    <div style={{ textAlign: "center", fontWeight: 600 }}>
+      A solo rogue-like card game
+    </div>
     <h1 style={{ textAlign: "center", margin: "0" }}>Scoundrel Solitaire</h1>
 
-    <div className={styles.suites}>
-      <div style={{ color: "var(--black)" }}>♣</div>
-      <div style={{ color: "var(--red)" }}>♥ </div>
-      <div style={{ color: "var(--black)" }}>♠</div>
-      <div style={{ color: "var(--red)" }}>♦</div>
-    </div>
-
     <div>
-      Scoundrel is a solo rogue-like card game. Each room is made up of four
-      cards representing monsters, weapons, and healing items. You must choose
-      three cards to resolve in any order and carry the last card to the next
-      room.
+      In Scoundrel each room is made up of four cards representing monsters,
+      weapons, and healing items. You must choose three cards to resolve in any
+      order and carry the last card to the next room.
     </div>
 
     <div>
@@ -29,6 +26,33 @@ export const Introduction: FC = () => (
     <div>
       The game ends when you die (your health hits zero) or survive through the
       deck. Success depends on smart risk-taking and order of play.
+    </div>
+
+    <div className={styles.suites}>
+      <div
+        className={styles.suite}
+        style={{ [COLOR]: "var(--black)" } as CSSProperties}
+      >
+        ♣
+      </div>
+      <div
+        className={styles.suite}
+        style={{ [COLOR]: "var(--red)" } as CSSProperties}
+      >
+        ♥{" "}
+      </div>
+      <div
+        className={styles.suite}
+        style={{ [COLOR]: "var(--black)" } as CSSProperties}
+      >
+        ♠
+      </div>
+      <div
+        className={styles.suite}
+        style={{ [COLOR]: "var(--red)" } as CSSProperties}
+      >
+        ♦
+      </div>
     </div>
   </div>
 );
